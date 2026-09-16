6a9668888af0b12002512cc9e451725ca7d0d8b1
Vikash Saroj <vikashsaroj436@gmail.com>
2026-09-17 01:24:20 +0530

    improving app.jsx for better interface

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-
+import { Routes } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import ChatBox from './components/ChatBox'
+import Credits from './pages/Credits'
+import Community from './pages/Community'
 const App = () => {
   return (
     <>
     <div>
+    <div className='flex h-screen w-screen'>
       <Sidebar/>
-      <Routes/>
+      <Routes>
+     <Route path='/' elements={<ChatBox />}/>
+     <Route path='/credits' elements={<Credits/>}/>
+     <Route path='/community' elements={<Community/>}/>
+      </Routes>
+     </div>
     </div>
-
     </>
   )
 }
