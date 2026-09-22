9399e573cc33d66b6b7789823ea69ebdd992ba81
Vikash Saroj <vikashsaroj436@gmail.com>
2026-09-22 18:21:57 +0530

    adding usereffects onto the appcontext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,16 +1,67 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyUserData } from "../assets/assets";
 
-const AppContext = createContext()
+const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
- 
-  const value = {}
+  const navigate = useNavigate();
 
+  const [user, setUser] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "light"
+  );
+
+  const fetchUser = async () => {
+    setUser(dummyUserData);
+  }
+  const fetchUserChats = async () => {
+      setChats(dummyChats)
+      setSelectedChat(dummyChats[0])
+    }
+    useEffect(()=>{
+      if(user){
+        fetchUserChats()
+      }
+      else{
+        setChats([])
+        setSelectedChat(null)
+      }
+    },[user])
+useEffect(() => {
+if(theme==='dark'){
+  document.documentElement.classList.add('dark');
+}else{
+  document.documentElement.classList.remove('dark');
+}
+},[theme])
+
+
+
+  useEffect(() => {
+    fetchUser();
+    fetchUserChats();
+  }, []);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    chats,
+    setChats,
+    selectedChat,
+    setSelectedChat,
+    theme,
+    setTheme,
+  };
 
   return (
-    <AppContext.Provider value={{}}>
+    <AppContext.Provider value={value}>
       {children}
     </AppContext.Provider>
-  )
-}
-export const useAppContext = () => useContext(AppContext)
+  );
+};
+
+export const useAppContext = () => useContext(AppContext);
